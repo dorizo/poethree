93d45024901869b146ab6934e20a2629f98da306
doris hermawan <dorizo89@gmail.com>
2023-08-18 13:11:39 +0700

    sss

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/moren/';
+    public $baseURL = 'https://poethree.antersekolah.com/';
 
     /**
      * --------------------------------------------------------------------------
